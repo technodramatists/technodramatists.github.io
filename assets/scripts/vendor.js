58969ee407581b54e873c2e3262bba7966ec23c6
Justinayeung <42782126+Justinayeung@users.noreply.github.com>
2021-07-14 17:14:12 -0400

    Paralella

---
+++ assets/scripts/vendor.js
@@ -0,0 +1,17 @@
+//Divs
+const playerDiv = document.getElementById('stream-container-inner');
+const container = document.getElementById('container');
+const controlsDiv = document.getElementById('control-buttons');
+
+//HTML Elements
+const overlayBox = document.getElementById('overlay-box');
+const overlayTextOne = document.getElementById('overlay-text-1');
+const overlayTextTwo = document.getElementById('overlay-text-2');
+const streamPlayerDiv = document.getElementById('stream-player');
+
+//Buttons
+const playVidBtn = document.getElementById('btn-vid-play');
+const switchBtn = document.getElementById('btn-arr-switch');
+const muteBtn = document.getElementById('btn-mute-video');
+const srcChangeBtn = document.getElementById('btn-src-change');
+const maxScreenBtn = document.getElementById('btn-max-screen');
